29d59955ea9cbd410818c789686cea52c44fe786
NailGalimov <nailgalimov8@gmail.com>
2022-11-03 11:30:57 +0300

    upd

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,4 @@
 const server = 'https://admin.ricc-it.com';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY0NTQxNjIxLCJleHAiOjE2NjcxMzM2MjF9.JRslLB3CdMDI6Y0kxnnuN-teIIXEVQGLEUUxXwh_7BI';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY3NDY0MjA0LCJleHAiOjE2NzAwNTYyMDR9.rS8tNEqFK3eFqy6uG-S1YlQpcoA4L4ljX0Q3d-Iz4Ag';
 
 export {server, token};
