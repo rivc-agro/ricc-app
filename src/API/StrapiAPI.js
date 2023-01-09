8330585a0902842766e59e0bcc5b2484230c47f1
NailGalimov <nailgalimov8@gmail.com>
2023-01-09 20:08:33 +0300

    upd

--- src/API/StrapiAPI.js
+++ src/API/StrapiAPI.js
@@ -3,74 +3,42 @@ import { token, server } from "../data/data";
 
 export default class StrapiAPI {
     static async getLogo() {
-        const response = await axios.get(`${server}/api/contents?populate=*`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
+        const response = await axios.get(`${server}/api/contents?populate=*`)
         return response.data;
     }
 
     static async getIntro() {
-        const response = await axios.get(`${server}/api/intro-sections`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
+        const response = await axios.get(`${server}/api/intro-sections`)
         return response.data;
     }
 
     static async getBenefits() {
-        const response = await axios.get(`${server}/api/benefits?populate=*`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
+        const response = await axios.get(`${server}/api/benefits?populate=*`)
         return response.data;
     }
 
     static async getTypesSliders() {
-        const response = await axios.get(`${server}/api/type-sliders?populate=*`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
+        const response = await axios.get(`${server}/api/type-sliders?populate=*`)
         return response.data;
     }
 
     static async getSourses() {
-        const response = await axios.get(`${server}/api/data-sourses?populate=*`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
+        const response = await axios.get(`${server}/api/data-sourses?populate=*`)
         return response.data;
     }
 
     static async getDashboard() {
-        const response = await axios.get(`${server}/api/dashbords?populate=*`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
+        const response = await axios.get(`${server}/api/dashbords?populate=*`)
         return response.data;
     }
 
     static async getSocials() {
-        const response = await axios.get(`${server}/api/socials`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
+        const response = await axios.get(`${server}/api/socials`)
         return response.data;
     }
 
     static async getWorks() {
-        const response = await axios.get(`${server}/api/how-we-works`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
+        const response = await axios.get(`${server}/api/how-we-works`)
         return response.data;
     }
 }
